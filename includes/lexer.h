bdfc11a59b7452bf2cd1716a92951aeef788f8d5
Fábio Almeida <fabialme@c1r8p3.42sp.org.br>
2025-11-25 11:37:15 -0300

    feat: create lexer header with struct

---
+++ includes/lexer.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fabialme <fabialme@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/25 11:32:06 by fabialme          #+#    #+#             */
+/*   Updated: 2025/11/25 11:36:26 by fabialme         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEXER_H
+# define LEXER_H
+
+typedef enum e_token_type
+{
+  TOKEN_WORD,
+  TOKEN_PIPE,
+  TOKEN_REDIR_IN,
+  TOKEN_REDIR_OUT,
+  TOKEN_REDIR_APPEND,
+  TOKEN_REDIR_HEREDOC,
+  TOKEN_AND,
+  TOKEN_OR,
+  TOKEN_PAREN_OPEN,
+  TOKEN_PAREN_CLOSE,
+  TOKEN_EOF
+} t_token_type;
+
+typedef struct s_token
+{
+    char            *value;
+    t_token_type    type;
+    struct s_token  *next;
+} t_token;
+
+#endif // !LEXER_H
