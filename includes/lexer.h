020a59074b1d37a4aa148f0355bbe862c8b75b33
Fábio Almeida <fabialme@c1r8p3.42sp.org.br>
2025-11-25 11:39:34 -0300

    fix: fixed norminette

--- includes/lexer.h
+++ includes/lexer.h
@@ -6,7 +6,7 @@
 /*   By: fabialme <fabialme@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 11:32:06 by fabialme          #+#    #+#             */
-/*   Updated: 2025/11/25 11:36:26 by fabialme         ###   ########.fr       */
+/*   Updated: 2025/11/25 11:39:20 by fabialme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,24 @@
 
 typedef enum e_token_type
 {
-  TOKEN_WORD,
-  TOKEN_PIPE,
-  TOKEN_REDIR_IN,
-  TOKEN_REDIR_OUT,
-  TOKEN_REDIR_APPEND,
-  TOKEN_REDIR_HEREDOC,
-  TOKEN_AND,
-  TOKEN_OR,
-  TOKEN_PAREN_OPEN,
-  TOKEN_PAREN_CLOSE,
-  TOKEN_EOF
-} t_token_type;
+	TOKEN_WORD,
+	TOKEN_PIPE,
+	TOKEN_REDIR_IN,
+	TOKEN_REDIR_OUT,
+	TOKEN_REDIR_APPEND,
+	TOKEN_REDIR_HEREDOC,
+	TOKEN_AND,
+	TOKEN_OR,
+	TOKEN_PAREN_OPEN,
+	TOKEN_PAREN_CLOSE,
+	TOKEN_EOF
+}	t_token_type;
 
 typedef struct s_token
 {
-    char            *value;
-    t_token_type    type;
-    struct s_token  *next;
-} t_token;
+	char			*value;
+	t_token_type	type;
+	struct s_token	*next;
+}	t_token;
 
 #endif // !LEXER_H
